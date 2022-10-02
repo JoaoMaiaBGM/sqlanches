c362a295a7ba9f24c6555ea8c75d64480623e4d3
João Maia <joaocarlosbgm@gmail.com>
2022-10-02 19:58:03 -0300

    feat: crud / Read

--- sql/crud.sql
+++ sql/crud.sql
@@ -58,3 +58,32 @@ VALUES
       FROM produtos p2 
       WHERE p2.nome = 'Coca-Cola'
       ));
+
+--LEITURA
+--2)Selecione os dados da tabela clientes, juntamente com os pedidos e os produtos dos pedidos da cliente Georgia.
+SELECT 
+	c.id,
+	c.nome,
+	c.lealdade,
+	p.id, 
+	p.status,
+	p.cliente_id,
+	p2.id,
+	p2.nome,
+	p2.tipo,
+	ROUND(CAST(p2.preço AS NUMERIC), 2) AS preço,
+	p2.pts_de_lealdade
+FROM 
+	produtos_pedidos pp 
+JOIN 
+	pedidos p ON p.id = pp.pedido_id 
+JOIN 
+	clientes c ON c.id = p.cliente_id 
+JOIN 
+	produtos p2 ON p2.id = pp.produto_id 
+WHERE 
+	pp.pedido_id = (
+		SELECT c.id
+		FROM clientes c
+		WHERE c.nome = 'Georgia'
+	);
